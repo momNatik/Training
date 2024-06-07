687bf3c1377d17710776e330f3cc98bd277f9fc3
Nataliya <mom.natik@gmail.com>
2024-06-07 13:37:49 +0300

    renaming variables

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -1,14 +1,14 @@
-function* PointsOnCircle(n) {
+function* GetPointsOfCircle(n) {
     const r = 1;
-    let corner = Math.PI*2 / n;
+    let deltaAngle = Math.PI * 2 / n;
     let point = [1, 0];
     let index = 0;
-    let cornerOfPoint = corner;
+    let cornerOfPoint = deltaAngle;
     while (index < n) {
         if (index > 0) {
             point[0] = r * Math.cos(cornerOfPoint).toFixed(2);
             point[1] = r * Math.sin(cornerOfPoint).toFixed(2);
-            cornerOfPoint += corner;
+            cornerOfPoint += deltaAngle;
         }
         yield point;
         index++;
