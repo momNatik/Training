740b6cf369b3e794ce18d27ebe69ddf11ab82a54
momNatik <105671628+momNatik@users.noreply.github.com>
2024-06-10 16:00:25 +0300

    add center coordinates (#3)
    * add center coordinates
    
    * rename arguments x0 > x, y0 > y

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -1,17 +1,16 @@
-function* GetPointsOfCircle(n) {
-    const r = 1;
+function* GetPointsOfCircle(x, y, r, n) {
     let deltaAngle = Math.PI * 2 / n;
     let angle = 0;
 
     for (let i = 0; i < n; i++) {
         let point = {
-            x: r * Math.cos(angle),
-            y: r * Math.sin(angle)
+            x: x + r * Math.cos(angle),
+            y: y + r * Math.sin(angle)
         };
         yield point;
         angle += deltaAngle; 
     }
 }
-const points = Array.from(GetPointsOfCircle(5))
+const points = Array.from(GetPointsOfCircle(2, 1, 3, 5))
 
 console.log("Points: ", points);
