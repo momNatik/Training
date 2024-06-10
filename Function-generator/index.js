3521b1a9c12402a6235751dc490bc9fe113f13dd
Nataliya <mom.natik@gmail.com>
2024-06-10 15:59:10 +0300

    rename arguments x0 > x, y0 > y

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -1,16 +1,16 @@
-function* GetPointsOfCircle(x0, y0, r, n) {
+function* GetPointsOfCircle(x, y, r, n) {
     let deltaAngle = Math.PI * 2 / n;
     let angle = 0;
 
     for (let i = 0; i < n; i++) {
         let point = {
-            x: x0 + r * Math.cos(angle),
-            y: y0 + r * Math.sin(angle)
+            x: x + r * Math.cos(angle),
+            y: y + r * Math.sin(angle)
         };
         yield point;
         angle += deltaAngle; 
     }
 }
-const points = Array.from(GetPointsOfCircle(0, 0, 1, 5))
+const points = Array.from(GetPointsOfCircle(2, 1, 3, 5))
 
 console.log("Points: ", points);
