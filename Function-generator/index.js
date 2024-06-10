02b910c927ad3ef82a86d22677c756dd12fbde48
Nataliya <mom.natik@gmail.com>
2024-06-10 15:34:05 +0300

    fixes by cod review

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -1,19 +1,17 @@
 function* GetPointsOfCircle(n) {
     const r = 1;
     let deltaAngle = Math.PI * 2 / n;
-    let angle = deltaAngle;
-    let point = {
-        x: 1,
-        y: 0
-    }
+    let angle = 0;
+
     for (let i = 0; i < n; i++) {
+        let point = {
+            x: r * Math.cos(angle),
+            y: r * Math.sin(angle)
+        };
         yield point;
-        point.x = r * Math.cos(angle);
-        point.y = r * Math.sin(angle);
-        angle += deltaAngle;
+        angle += deltaAngle; 
     }
 }
-
 const points = Array.from(GetPointsOfCircle(5))
 
 console.log("Points: ", points);
