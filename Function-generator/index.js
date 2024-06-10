8b068102ba027e1e71fe6c214365869b261111de
Nataliya <mom.natik@gmail.com>
2024-06-10 16:36:04 +0300

    return the segment between the points

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -3,14 +3,16 @@ function* GetPointsOfCircle(x, y, r, n) {
     let angle = 0;
 
     for (let i = 0; i < n; i++) {
-        let point = {
-            x: x + r * Math.cos(angle),
-            y: y + r * Math.sin(angle)
-        };
-        yield point;
+        let segment = {
+            x0: x + r * Math.cos(angle),
+            y0: y + r * Math.sin(angle),
+            x1: x + r * Math.cos(angle + deltaAngle),
+            y1: y + r * Math.sin(angle + deltaAngle)
+        }
+        yield segment;
         angle += deltaAngle; 
     }
 }
-const points = Array.from(GetPointsOfCircle(2, 1, 3, 5))
+const points = Array.from(GetPointsOfCircle(0, 0, 1, 4))
 
 console.log("Points: ", points);
