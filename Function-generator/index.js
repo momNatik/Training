2399413b7d78c54e31f282e3a9bed9d9bca21eba
Nataliya <mom.natik@gmail.com>
2024-06-11 09:52:50 +0300

    save previous point

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -1,16 +1,24 @@
 function* GetPointsOfCircle(x, y, r, n) {
     let deltaAngle = Math.PI * 2 / n;
     let angle = 0;
+    let previousPoint = {
+        x: x + r * Math.cos(angle),
+        y: y + r * Math.sin(angle)
+    }
 
     for (let i = 0; i < n; i++) {
         let segment = {
-            x0: x + r * Math.cos(angle),
-            y0: y + r * Math.sin(angle),
+            x0: previousPoint.x,
+            y0: previousPoint.y,
             x1: x + r * Math.cos(angle + deltaAngle),
             y1: y + r * Math.sin(angle + deltaAngle)
         }
         yield segment;
-        angle += deltaAngle; 
+        previousPoint = {
+            x: segment.x1,
+            y: segment.y1
+        }
+        angle += deltaAngle;
     }
 }
 const points = Array.from(GetPointsOfCircle(0, 0, 1, 4))
