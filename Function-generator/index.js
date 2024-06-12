3c345b452ac2d1002e50ff2852e843d60d5370db
momNatik <105671628+momNatik@users.noreply.github.com>
2024-06-12 12:29:52 +0300

    Separate function to generate segments (#5)
    * Separate function to generate segments
    
    * Updated by code review

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -12,18 +12,41 @@ function* GetPointsOfCircle(x, y, r, n) {
     let p0 = calcPoint(angle);
 
     for (let i = 0; i < n; i++) {
+        let point = {
+            x: x + r * Math.cos(angle),
+            y: y + r * Math.sin(angle)
+        };
+        yield point;
         angle += deltaAngle;
-        const p1 = calcPoint(angle);
-        const segment = {
+    }
+}
+
+function* GetSegmenstFromPoints(points) {
+    let current = points.next();
+
+    if (current.done) {
+        return;
+    }
+
+    let p0 = current.value;
+
+    current = points.next();
+    while (!current.done) {
+        const p1 = current.value;
+        yield {
             x0: p0.x,
             y0: p0.y,
             x1: p1.x,
             y1: p1.y
-        }
-        yield segment;
+        };
+
         p0 = p1;
+
+        current = points.next();
     }
 }
-const points = Array.from(GetPointsOfCircle(0, 0, 1, 4))
 
-console.log("Points: ", points);
+const points = GetPointsOfCircle(2, 1, 3, 5);
+const segments = GetSegmenstFromPoints(points);
+
+console.log("Segments: ", [...segments]);
