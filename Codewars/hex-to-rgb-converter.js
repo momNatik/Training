1eca6034585324372f563eeefda1953fc1f7cdfa
Nataliya <mom.natik@gmail.com>
2024-06-12 11:34:59 +0300

    renaming

--- Codewars/hex-to-rgb-converter.js
+++ Codewars/hex-to-rgb-converter.js
@@ -1,14 +1,14 @@
-function ConverterHexToRgb(hexString) {
+function HexToRgb(hexString) {
     const rgb = {
         r: 0,
         g: 0,
         b: 0
     }
-    const trimHexString = hexString.toLowerCase().substring(1);
+    const hexOnly = hexString.toLowerCase().substring(1);
 
-    rgb.r = parseInt(trimHexString.substring(0, 2), 16);
-    rgb.g = parseInt(trimHexString.substring(2, 4), 16);
-    rgb.b = parseInt(trimHexString.substring(4), 16);
+    rgb.r = parseInt(hexOnly.substring(0, 2), 16);
+    rgb.g = parseInt(hexOnly.substring(2, 4), 16);
+    rgb.b = parseInt(hexOnly.substring(4), 16);
 
     return rgb;
 }
