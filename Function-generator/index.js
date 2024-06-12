0a9b8d0a0550d708fe00c446f544944f3c36324a
Nataliya <mom.natik@gmail.com>
2024-06-12 10:04:20 +0300

    Separate function to generate segments

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -13,4 +13,18 @@ function* GetPointsOfCircle(x, y, r, n) {
 }
 const points = Array.from(GetPointsOfCircle(2, 1, 3, 5))
 
-console.log("Points: ", points);
+function* GetSegmenstFromPoints(points) {
+   for (let i = 0; i < points.length - 1; i++) {
+    let segment = {
+        x0: points[i].x,
+        y0: points[i].y,
+        x1: points[i+1].x,
+        y1: points[i+1].y
+    }
+    yield segment;
+   }
+}
+
+const segments = Array.from(GetSegmenstFromPoints(points));
+
+console.log("Segments: ", segments);
