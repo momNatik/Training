daf81173650eb910b3be39398e30b67f5de9f1c8
Nataliya <mom.natik@gmail.com>
2024-06-12 11:29:11 +0300

    WIP

--- Codewars/dice_score.js
+++ Codewars/dice_score.js
@@ -1,20 +1,41 @@
 function score(arrayOfNum) {
-const array = arrayOfNum;
-let counterOfPoints = 0;
+    let counterOfPoints = 0;
+    const sortArray = arrayOfNum.sort((a, b) => a - b);
+    const indexOfNum = [];
 
-function GetValueOfNum(number, array) {
-   const counterOfNumbers = array.filter((num) => num == number);
-   console.log({number: counterOfNumbers});
-    return {number: counterOfNumbers}
-} 
+    for (let i = 1; i <= 6; i++) {
+        const index = (element) => element == i;
+        indexOfNum[i - 1] = sortArray.findLastIndex(index);
+        if (indexOfNum[i - 1] != -1) {
+            if (i == 1) {
+                if (indexOfNum[i - 1] == 2) {
+                    counterOfPoints += 1000;
+                } else if (indexOfNum[i - 1] == 0) {
+                    counterOfPoints += 100;
+                }
+            }
+            if (i == 2) {
+                if (indexOfNum[i - 1] == 2) {
+                    counterOfPoints += 200;
+                }
+            }
+        }
+    }
 
-for (let i = 1; i <= 6; i++) {
-    const valuesOfNum = Array.from(GetValueOfNum(i,array));
+    return counterOfPoints;
+}
+
+console.log(score([4, 1, 4, 3, 3]))
+
+
+    // function GetValueOfNum(number, array) {
+    //     const counterOfNumbers = array.filter((num) => num == number);
+    //     const obj = {};
+    //     obj[number] = counterOfNumbers;
+    //     return obj;
+    // }
+    // const valuesOfNum = [];
+    // for (let i = 1; i <= 6; i++) {
+    //     valuesOfNum[i - 1] = GetValueOfNum(i, array);
+    // }
     // console.log(valuesOfNum);
-}
-
-
-// return counterOfPoints;
-}
-
-console.log(score([4,1,4,3,3]))
