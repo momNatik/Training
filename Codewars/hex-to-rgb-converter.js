e048494dc69fc025d7e69ab8c976003d437400dc
Nataliya <mom.natik@gmail.com>
2024-06-12 11:57:19 +0300

    Updated by code review

--- Codewars/hex-to-rgb-converter.js
+++ Codewars/hex-to-rgb-converter.js
@@ -1,16 +1,11 @@
 function HexToRgb(hexString) {
-    const rgb = {
-        r: 0,
-        g: 0,
-        b: 0
-    }
     const hexOnly = hexString.toLowerCase().substring(1);
 
-    rgb.r = parseInt(hexOnly.substring(0, 2), 16);
-    rgb.g = parseInt(hexOnly.substring(2, 4), 16);
-    rgb.b = parseInt(hexOnly.substring(4), 16);
-
-    return rgb;
+    return {
+        r: parseInt(hexOnly.substring(0, 2), 16),
+        g: parseInt(hexOnly.substring(2, 4), 16),
+        b: parseInt(hexOnly.substring(4), 16)
+    }
 }
 
-console.log(Converter('#FF9933'));
+console.log(HexToRgb('#FF9933'));
