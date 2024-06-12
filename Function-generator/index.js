80d3955e3c422101b545594444ee9178c2c46c14
momNatik <105671628+momNatik@users.noreply.github.com>
2024-06-12 11:51:13 +0300

    return the segment between the points (#4)
    * return the segment between the points
    
    * save previous point
    
    * Updated by code review

--- Function-generator/index.js
+++ Function-generator/index.js
@@ -2,15 +2,28 @@ function* GetPointsOfCircle(x, y, r, n) {
     let deltaAngle = Math.PI * 2 / n;
     let angle = 0;
 
+    function calcPoint(a) {
+        return {
+            x: x + r * Math.cos(a),
+            y: y + r * Math.sin(a)
+        }
+    }
+
+    let p0 = calcPoint(angle);
+
     for (let i = 0; i < n; i++) {
-        let point = {
-            x: x + r * Math.cos(angle),
-            y: y + r * Math.sin(angle)
-        };
-        yield point;
-        angle += deltaAngle; 
+        angle += deltaAngle;
+        const p1 = calcPoint(angle);
+        const segment = {
+            x0: p0.x,
+            y0: p0.y,
+            x1: p1.x,
+            y1: p1.y
+        }
+        yield segment;
+        p0 = p1;
     }
 }
-const points = Array.from(GetPointsOfCircle(2, 1, 3, 5))
+const points = Array.from(GetPointsOfCircle(0, 0, 1, 4))
 
 console.log("Points: ", points);
